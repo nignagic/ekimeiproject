0061cd43d3e8ad35eab8517c20b963ac2be482b0
nignagic <icnignag@gmail.com>
2020-06-06 06:25:05 +0900

    for prototype release

--- songdata/admin.py
+++ songdata/admin.py
@@ -1,7 +1,13 @@
 from django.contrib import admin
 from .models import *
 
+class SongAdmin(admin.ModelAdmin):
+	list_display = ('name', 'name_kana')
+
+class ArtistAdmin(admin.ModelAdmin):
+	list_display = ('name', 'name_kana')
+
 # Register your models here.
-admin.site.register(Artist)
-admin.site.register(Song)
+admin.site.register(Artist, ArtistAdmin)
+admin.site.register(Song, SongAdmin)
 admin.site.register(VocalNew)
