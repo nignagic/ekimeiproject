97f4afd85ca9166240e82a7fc73a426ecb0986ce
nignagic <icnignag@gmail.com>
2021-05-29 20:28:34 +0900

    config collectstatic

--- ekimeiproject/settings/base.py
+++ ekimeiproject/settings/base.py
@@ -115,8 +115,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
+
+STATIC_ROOT = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), 'static')
 
 AUTH_USER_MODEL = 'user.User'
 
