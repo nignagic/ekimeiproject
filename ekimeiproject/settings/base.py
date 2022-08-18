b0ed9b3fa22e415d5641a24157ababf5c1c11aff
nignagic <icnignag@gmail.com>
2022-08-19 00:00:36 +0900

    enabled debug_toolbar

--- ekimeiproject/settings/base.py
+++ ekimeiproject/settings/base.py
@@ -51,11 +51,6 @@ MIDDLEWARE = [
     'debug_toolbar.middleware.DebugToolbarMiddleware',
 ]
 
-INTERNAL_IPS = ['127.0.0.1']
-DEBUG_TOOLBAR_CONFIG = {
-    "SHOW_TOOLBAR_CALLBACK" : lambda request: True,
-}
-
 ROOT_URLCONF = 'ekimeiproject.urls'
 
 TEMPLATES = [
