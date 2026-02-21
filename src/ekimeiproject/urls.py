2eb02866464229b0053fff0ea8b1de28fa52fd39
nignagic <icnignag@gmail.com>
2026-02-21 21:24:54 +0900

    fix for exec in local

--- src/ekimeiproject/urls.py
+++ src/ekimeiproject/urls.py
@@ -15,7 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.conf import settings
-from django.urls import path, include
+from django.urls import path, include, re_path
+from django.views.static import serve
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -30,3 +31,11 @@ if settings.DEBUG:
     urlpatterns = [
         path('__debug__/', include(debug_toolbar.urls)),
     ] + urlpatterns
+
+# When running local docker with DEBUG=False, Django's `static()` helper
+# does not register routes. Serve static/media explicitly for local use.
+if not settings.DEBUG:
+    urlpatterns += [
+        re_path(r"^static/(?P<path>.*)$", serve, {"document_root": settings.STATIC_ROOT}),
+        re_path(r"^media/(?P<path>.*)$", serve, {"document_root": settings.MEDIA_ROOT}),
+    ]
